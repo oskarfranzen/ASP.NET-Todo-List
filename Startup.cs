a8363cd5a7f7ff40af797f38aaa02c50466de753
Oskar Franzén <oskarfranzen93@gmail.com>
2018-01-17 20:14:38 +0100

    Inital Commit with code

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+
+namespace TodoApi {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoItems"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            app.UseMvc();
+        }
+    }
+}
