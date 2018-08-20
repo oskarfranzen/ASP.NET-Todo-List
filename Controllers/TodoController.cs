702bac9deee5521e6ad38a8c28590786ee5afda4
Oskar Franzén <oskarfranzen93@gmail.com>
2018-08-20 22:09:14 +0200

    Fix incorrect parameter when returning uri to item created by POST

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -12,8 +13,8 @@ namespace TodoApi.Controllers {
             _context = context;
 
             if (_context.TodoItems.Count() == 0) {
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-                _context.TodoItems.Add(new TodoItem { Name = "Item2" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item22", Alert = new Alert { TimeOfAlert = DateTime.Now.AddMinutes(5), AlertColor = System.Drawing.Color.Red } });
+                _context.TodoItems.Add(new TodoItem { Name = "Item2", Alert = new Alert { TimeOfAlert = DateTime.Now, AlertColor = System.Drawing.Color.Red } });
                 _context.SaveChanges();
             }
         }
@@ -40,7 +41,8 @@ namespace TodoApi.Controllers {
             _context.TodoItems.Add(itemToCreate);
             _context.SaveChanges();
 
-            return CreatedAtRoute("GetTodo", itemToCreate.Id, itemToCreate);
+            
+            return CreatedAtRoute("GetTodo", new {Id = itemToCreate.Id }, itemToCreate);
         }
 
         [HttpPut("{id}")]
