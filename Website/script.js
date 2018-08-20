6901019e584d9ce5a916a2a08766eb434ef41511
Oskar Franzén <oskarfranzen93@gmail.com>
2018-08-20 23:18:42 +0200

    Add newly added items to UI after POSTing to API

--- Website/script.js
+++ Website/script.js
@@ -1,4 +1,15 @@
 var API_URL = "http://localhost:5000/api/todo"
+
+class TodoObjectClass {
+    constructor(_id, _name, _isDone, _alert, _color) {
+        this.Id = _id;
+        this.Name = _name;
+        this.IsDone = _isDone;
+        this.Alert = _alert;
+        this.Color = _color;
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     InitializeEvents();
     updateDisplay();
@@ -10,25 +21,28 @@ function InitializeEvents() {
     var addForm = document.querySelector("#addnewItemForm")
     addForm.onsubmit = function (e) {
         e.preventDefault()
-        
+
         var requestObject = new XMLHttpRequest()
         requestObject.open('POST', API_URL)
-        
+
         var nameOfItem = document.querySelector("#addTodoItem");
 
         var body = JSON.stringify(
             {
-                name: nameOfItem.value, 
-                iscompleted:false
+                name: nameOfItem.value,
+                iscompleted: false
             }
         );
 
         requestObject.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
         requestObject.onload = function () {
-            updateDisplay();
+            var currentObject = JSON.parse(requestObject.response);
+            debugger;
+            var content = createNewTodoItemElement(new TodoObjectClass(currentObject.id, currentObject.name, currentObject.isDone, currentObject.alert.timeOfAlert, currentObject.alert.alertColor));
+            document.querySelector("#listOfItems").appendChild(content)
         }
-        requestObject.send(body)    
+        requestObject.send(body)
         nameOfItem.value = ""
     }
 }
@@ -40,24 +54,18 @@ function updateDisplay() {
     requestObject.responseType = "application/json";
 
     requestObject.onload = function () {
-        document.querySelector("#rawRetrievedData").innerHTML += requestObject.response
+        // document.querySelector("#rawRetrievedData").innerHTML += requestObject.response
 
         var arrJson = JSON.parse(requestObject.response);
         var formattedTodoList = document.querySelector("#listOfItems")
         formattedTodoList.innerHTML = ""
         for (i = 0; i < arrJson.length; i++) {
             var currentObject = arrJson[i];
-            var todoObject = {
-                Id: currentObject.id,
-                Name: currentObject.name,
-                IsDone: currentObject.isDone,
-                Alert: currentObject.alert.timeOfAlert,
-                Color: currentObject.alert.alertColor
-            }
+            var todoObject = new TodoObjectClass(currentObject.id, currentObject.name, currentObject.isDone, currentObject.alert.timeOfAlert, currentObject.alert.alertColor);
+            
             if (formattedTodoList != null) {
                 var content = createNewTodoItemElement(todoObject)
                 formattedTodoList.appendChild(content);
-
             }
         }
     };
@@ -67,10 +75,15 @@ function updateDisplay() {
 
 function createNewTodoItemElement(newObject) {
     var entry = document.createElement('li')
-    var concatProperties = "Id: " + newObject.Id + "</br>Name: " + newObject.Name + "</br>IsDone: " + (newObject.IsDone === true ? "√" : "x")
+    entry.className = "listItem"
+    var concatProperties = "Id: " + newObject.Id + ", Name: " + newObject.Name + ", IsDone: " + (newObject.IsDone === true ? "√" : "x")
     entry.innerHTML += "<p>" + concatProperties + "</p>"
     if (Date.parse(newObject.Alert) < (new Date()).getTime()) {
         entry.style.color = newObject.Color
     }
+    setTimeout(function () {
+        entry.className = entry.className + " show"
+    }, 100)
+
     return entry
 }
